7d4a8baa3c9489fd5a5d2f866374c9526a15f634
Benno2000 <benno.hoelz@gmx.de>
2023-02-28 22:25:30 +0100

    add SfzLoRa class

---
+++ library/SfzLoRa.h
@@ -0,0 +1,19 @@
+/*
+    SfzLoRa.h - Library for using the Dragino LA66 Shield in a quick and easy way.
+    Created by Benno Hölz, February 28, 2023.
+    Released into TODO
+*/
+#ifndef SfzLoRa_h
+#define SfzLoRa_h
+
+#include "Arduino.h"
+
+class SfzLoRa
+{
+    public:
+        SfzLoRa();
+        void send_bytes();
+    private:
+};
+
+#endif
